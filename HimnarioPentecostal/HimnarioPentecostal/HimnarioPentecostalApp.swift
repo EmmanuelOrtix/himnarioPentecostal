71ecb3cecb387bcd2ba079b8d9f72f73889ea7db
Emmanuel <oemmanuel617@gmail.com>
2021-06-28 20:21:49 -0400

    Initial project

---
+++ HimnarioPentecostal/HimnarioPentecostal/HimnarioPentecostalApp.swift
@@ -0,0 +1,17 @@
+//
+//  HimnarioPentecostalApp.swift
+//  HimnarioPentecostal
+//
+//  Created by Emmanuel Ortiz Matus on 28-06-21.
+//
+
+import SwiftUI
+
+@main
+struct HimnarioPentecostalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
