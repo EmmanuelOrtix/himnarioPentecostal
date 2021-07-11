fb1adebcbfb326e4966f5598b02858bd9fb6a921
Emmanuel <oemmanuel617@gmail.com>
2021-07-11 02:03:53 -0400

    add searcheable on listview himnos

--- HimnarioPentecostal/HimnarioPentecostal/Views/himnos/HimnosListView.swift
+++ HimnarioPentecostal/HimnarioPentecostal/Views/himnos/HimnosListView.swift
@@ -8,15 +8,20 @@
 import SwiftUI
 
 struct HimnosListView: View {
+    @State var text = ""
     var body: some View {
-        ZStack {
-            List(listaHimnario.sorted(by: { $0.numero < $1.numero })) { item in
+        VStack {
+            SearchBar(text: $text).background(Color(.systemGray6))
+            
+            List(listaHimnario.sorted(by: { $0.numero < $1.numero }).filter({
+                "\($0.numero)".contains(text) || text.isEmpty || "\($0.titulo)".contains(text.uppercased())
+            })) { item in
                 NavigationLink(
                     destination: HimnosContentView(model: item)){
                     Text(String(item.numero) + ".- " + item.titulo)
                 }
             }
-        }
+        }.background(Color(.systemGray6))
     }
 }
 
